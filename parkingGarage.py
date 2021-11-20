a0d3581dc4f0dadd8a56a87896fed367618a4c6b
WILL10CODE <wblumenthal100@gmail.com>
2021-11-19 22:58:30 -0600

    William's Update2

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,8 +1,8 @@
 class ParkingGarage():
     def __init__(self, size=100, occupied=50, tickets =[]):        
-        self.tickets = [1] * (size - occupied)
-        self.parkingSpaces = ['available'] * (size - occupied)
-        self.currentTicket = dict.fromkeys(["Parked"] * occupied],"Not Paid")
+        self.tickets = range(occupied,size)
+        self.parkingSpaces =range(occupied,size)
+        self.currentTicket = dict.fromkeys(range(occupied),"Not Paid")
         self.size = size
         self.occupied = occupied
 
@@ -10,18 +10,38 @@ class ParkingGarage():
     def takeTicket(self):
         if self.tickets:
             self.tickets.pop()
-            self.parkingSpaces.pop()
+            self.currentTicket[size - self.parkingSpaces.pop()] = "Not Paid"
         else:
             print("There are no available spaces")
         return
 
+    def ticketValid(self):
+        ticket_num = input("What is your ticket number? ")
+        while ticket_num not in self.currentTicket:
+            print("Invalid Ticket Number")
+            self.ticketValid()
+        return ticket_num
+
     def payforParking(self):
-        payment = Input("Please put in the dollar amount of your payment for parking:")
-            if not payment.isdigit():
-                print("Invalid entry. Please input in a number of dollars")
-                self.payforparking()
-        return
+        ticket_num = self.ticketValid()
+        payment = input("Please put in the dollar amount of your payment for parking:")
+        if not payment.isdigit():
+            print("Invalid entry. Please input in a number of dollars")
+            self.payforparking()
+
+        return payment
 
     def leaveGarage(self):
+        ticket_num = self.ticketValid()
+        if self.tickets:
+            print("Thank You, have a nice day!")
+        else:
+            print("Please make a payment for parking")
+            self.payforParking()
+            print("Thank You, have a nice day!")
+        self.tickets.append(1)
+        self.parkingSpaces.append('available')    
         return
+
+    
     
