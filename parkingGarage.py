762620800a31c853562e0b185d207b28a1e51978
WILL10CODE <wblumenthal100@gmail.com>
2021-11-19 21:12:23 -0600

    William's Update

---
+++ parkingGarage.py
@@ -0,0 +1,17 @@
+class ParkingGarage():
+    def __init__(self, tickets =[]):
+        self.tickets = tickets
+        self.parkingSpaces = []
+        self.currentTicket = {}
+
+
+    def takeTicket(self):
+        
+        return
+
+    def payforParking(self):
+        return
+
+    def leaveGarage(self):
+        return
+    
